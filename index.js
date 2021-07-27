9456b5730d074ba0729367e234c90ca79fcebf35
unknown <jtjackson325@gmail.com>
2021-07-27 17:48:55 -0600

    got the questions typed out for the different sections

--- index.js
+++ index.js
@@ -2,7 +2,97 @@
 const inquirer = require('inquirer');
 console.log(inquirer);
 // TODO: Create an array of questions for user input
-const questions = [];
+const userQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the title of your Project'
+        },
+        {
+            type: 'input',
+            name: 'how',
+            message: 'How did you come up with the idea for this project'
+        },
+        {
+            type: 'input',
+            name: 'motivation',
+            message: 'What motivated you to do this project'
+        },
+        {
+            type: 'input', 
+            name: 'objective',
+            message: 'What did you hope to achieve with this project'
+        },
+        {
+            type: 'input',
+            name: 'benefit',
+            message: 'How does your project benefit the world'
+        },
+        {
+            type: 'checkbox',
+            name: 'contents',
+            message: 'What would you like to include in you table of contents? (Check all that apply)',
+            choices: ['Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions']
+        }
+    ]);
+};
+
+const contentQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What steps are needed to install your project onto my computer'
+        },
+        {
+            type: 'input',
+            name: 'using',
+            message: 'How do we use your application correctly'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What license is used with your application? (Choose from the options below)',
+            choices: ['MIT', 'GNU', 'Apache', 'ISC']
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'How do I contirbute to your project'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What tests have you created for your project'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Can you describe how to run the tests for your applicaiton'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github username'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address'
+        },
+        {
+            type: 'input',
+            name: 'phone',
+            message: 'What is your phone number'
+        }
+    ]);
+};
+
+userQuestions()
+    .then(answers => console.log(answers))
+    .then(contentQuestions)
+    .then(contentAnswers => console.log(contentAnswers));
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -12,3 +102,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+ 
