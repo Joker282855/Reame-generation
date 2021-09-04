8c9cdfe02f1d38d27109207dc707adb177b97b13
unknown <jtjackson325@gmail.com>
2021-09-03 18:42:22 -0600

    got more parts of the markdown done

--- Project-Info/utils/generateMarkdown.js
+++ Project-Info/utils/generateMarkdown.js
@@ -14,7 +14,43 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `
   # ${data.title}
+  
+  # Description
+  ${data.how}
+  ${data.motivation}
+  ${data.matter}
+  
+  # Table of Contents
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[License](#license)
+  -[Contributing](#contributing)
+  -[Tests](#tests)
+  -[Questions](#questions)
 
+  # Installation
+  Read the instructions below to know how to install the application on your computer:
+  ${data.installatioin}
+
+  # Usage
+  Here are instructions on how ot use the application: 
+  ${data.usage}
+
+  # License
+  The license this application use is ${data.license}
+
+  # Contributing 
+  To contirbute to the project follow these instructions:
+  ${data.contribution}
+
+  # Tests
+  The type of tests to run on this application are listed below along with a description on how to run them.
+  ${data.tests}
+  ${data.description}
+
+  # Questions
+  
+  
 `;
 }
 
